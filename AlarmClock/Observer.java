9dfea3156c4305dbe8dfec60f5d59929bc99e4e5
suttha jongjaroen <6410615121@cn330>
2024-05-03 12:36:42 +0700

    add observer pattern

---
+++ AlarmClock/Observer.java
@@ -0,0 +1,5 @@
+package AlarmClock;
+
+public interface Observer {
+    public void update();
+}
---
+++ AlarmClock/Subject.java
@@ -0,0 +1,9 @@
+package AlarmClock;
+
+public interface Subject {
+    public void attach(Observer o);
+
+    public void detach(Observer o);
+
+    public void notifyObserver();
+}
