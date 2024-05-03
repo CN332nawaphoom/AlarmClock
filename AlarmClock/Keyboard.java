994589a679f0f5b706f4e63eafec67300bbc223c
suttha jongjaroen <6410615121@cn330>
2024-05-03 12:45:08 +0700

    update empty Keyboard.java

--- AlarmClock/Keyboard.java
+++ AlarmClock/Keyboard.java
@@ -1,15 +1,34 @@
 package AlarmClock;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Keyboard
  */
-public class Keyboard {
+public class Keyboard implements Subject{
+    List<Observer> alarmclocks= new ArrayList<Observer>();
+
     public void longPress(){
 
     }
 
     public void shortPress(){
-        
+
     }
+
+    public void attach(Observer alarmclock){
+
+    }
+
+    public void detach(Observer alarmClock){
+
+    }
+
+    public void notifyObserver(){
+
+    }
+
+
     
 }
