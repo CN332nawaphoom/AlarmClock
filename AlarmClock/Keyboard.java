fb157588f90eaabdfa3fd4d9f10c116712ea15ea
suttha jongjaroen <6410615121@cn330>
2024-05-03 13:52:28 +0700

    implement keyboard

--- AlarmClock/Keyboard.java
+++ AlarmClock/Keyboard.java
@@ -8,25 +8,31 @@ import java.util.List;
  */
 public class Keyboard implements Subject{
     List<Observer> alarmclocks= new ArrayList<Observer>();
+    private String event;
 
+    // TODO add methods body
     public void longPress(){
-
+        event = "longPress";
+        notifyObserver();
     }
 
     public void shortPress(){
-
+        event = "shortPress";
+        notifyObserver();
     }
 
     public void attach(Observer alarmclock){
-
+        alarmclocks.add(alarmclock);
     }
 
     public void detach(Observer alarmClock){
-
+        alarmclocks.remove(alarmClock);
     }
 
     public void notifyObserver(){
-
+        for (Observer alarmclock : alarmclocks) {
+            alarmclock.update(event);
+        }
     }
 
 
